5d3ec2126ee666aeeb7048d877c3d196ba847df1
Mateusz Dziwina <mateuszdziwina@MBP-von-Mateusz.fritz.box>
2025-01-02 22:46:18 +0100

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <div className="bg-blue-50 text-black text-center py-4 mt-12">
+      <p className="text-sm">
+        Powered by <span className="font-semibold">Chase/Ryze*</span>/<span className="font-semibold">Mateusz</span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
